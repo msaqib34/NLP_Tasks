c87040b1923e59ff29f67c3b1b2e62ca7b8962c9
Muhammad Saqib Ijaz <muhammadsaqibijazkhan@gmail.com>
2026-05-11 15:58:46 +0500

    Re File Updated

--- regularexp.py
+++ regularexp.py
@@ -2,41 +2,47 @@ import re
 
 
 # Searching for a pattern in a string
-# input_string = input("Enter a string: ")
-
-# print(re.search("python",input_string,re.IGNORECASE))
+input_string = input("Enter a string: ")
+print(re.search("python",input_string,re.IGNORECASE))
 
 
 # Use of IgnoreCase flag
-# text = "Cat,cat,CAT,CAt,dog"
-# print(text.count("cat"))
-# print(re.findall("cat", text, re.IGNORECASE))
+text = "Cat,cat,CAT,CAt,dog"
+print(re.findall("cat", text, re.IGNORECASE))
 
-# for_split = "oneAndTwoAndThreeandFour"
-# print(re.split("and", for_split, flags=re.IGNORECASE))
+# Split on The Word "and"
+for_split = "oneAndTwoAndThreeandFour"
+print(re.split("and", for_split, flags=re.IGNORECASE))
 
-# paragraph = """This is a sample paragraph. 
-# It contains multiple sentences. 
-# Let's see how it works!"""
+
+# Split Paragraph into Sentences
+paragraph = """This is a sample paragraph. 
+It contains multiple sentences. 
+Let's see how it works!"""
  
-# # print(re.findall(r'\b\w+\b', paragraph,re.MULTILINE))
+print(re.findall(r'\b\w+\b', paragraph,re.MULTILINE))
 # # sentences = re.split(r'[.!?]+', paragraph) 
 # # sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
 # # print(sentences)
 # print(paragraph[0:4])
 
 
-# greetings = "Hello, how are you? Hello again!"
-# matches = re.sub(r"Hello","Hi", greetings)
-# print(matches)
+# Replaceing Words in a String
+greetings = "Hello, how are you? Hello again!"
+matches = re.sub(r"Hello","Hi", greetings)
+print(matches)
 
 # qu = "quantitative and qualitative research"
 # print(re.findall("quantitative", qu))
 
+# Extracting words from a String
+
 txt= "Code 87686 of Lahore city  42"
-# remove_number= re.sub(r"\d+", "", txt)
-# remove_space= re.sub(r"\s+", " ", remove_number)
-# print(remove_space)
+remove_number= re.sub(r"\d+", "", txt)
+remove_space= re.sub(r"\s+", " ", remove_number)
+print(remove_space)
+
+# Extracting Numbers from a String and Extracting Two Digit Numbers
 
 # print(re.findall(r"[a-z,A-Z]+", txt))
 print(re.sub(r"[^\d]", " ",txt))
